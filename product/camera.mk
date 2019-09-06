7c5ab28b72f6ec87ebcc58460613ef9e5bb7c5e4
John Vincent Corcega <github@tenseventyseven.uu.me>
2019-09-06 18:06:26 +0800

    Re-add Camera HAL

--- product/camera.mk
+++ product/camera.mk
@@ -1,4 +1,17 @@
-# Permissions
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm8937 \
+    libmm-qcamera
+	
+# Camera HAL
+PRODUCT_PACKAGES += \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
+
 # Snap
 PRODUCT_PACKAGES += \
     Snap
